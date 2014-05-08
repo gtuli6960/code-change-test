ad1a12804a85be19a2a1fa9ebfb1cd1c02c8c74a
George Tuli <gtuli6960@sohamcollege.org.uk>
2014-05-08 18:02:42 +0100

    Update greenBottles.py
    Add some comments to explain the code.

--- greenBottles.py
+++ greenBottles.py
@@ -1,17 +1,24 @@
+# Import the time library so delays can be added to the program.
 import time
+# Ask the user how many bottles to start with.
 bottles = int(input("How many green bottles are hanging on the wall?: "))
+# Prin the verses from the specified number of bottles to 0 bottles.
 for i in range(bottles,0,-1):
+    # Lanuguage change to account for '1 green bottle'.
     if i == 1:
         for j in range(2):
             print("1 green bottle hanging on the wall,")
         print("And if 1 green bottle should accidently fall,")
         print("There'll be 0 green bottles hanging on the wall.\n")
+    # Otherwise print the normal verses.
     else:
         for j in range(2):
             print("{0} green bottles hangning on the wall,".format(i))
         print("And if 1 green bottle should accidently fall,")
+        # Lanuguage change to account for '1 green bottle'.
         if i == 2:
             print("There'll be 1 green bottle hanging on the wall.\n".format(i-1))
         else:
             print("There'll be {0} green bottles hanging on the wall.\n".format(i-1))
+    # Time delay between printing verses.
     time.sleep(1)
