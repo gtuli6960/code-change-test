dc317f3688d5d5b71de2e802e46710194a73579a
gtuli6960 <gtuli6960@sohamcollege.org.uk>
2014-05-08 11:47:09 +0100

    Improved greenBottles.py
    I made the program count down from 10 to 0 green bottles, and made the code more efficient by using fewer lines.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,7 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+for i in range(10,0,-1):
+    for j in range(2):
+        print("{0} green bottles hangning on the wall,".format(i))
+    print("And if 1 green bottle should accidently fall,")
+    print("There'll be {0} green bottles hanging on the wall.\n".format(i-1))
+    time.sleep(1)
