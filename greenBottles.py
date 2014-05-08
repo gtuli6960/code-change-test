c39178e418131ec8ca969eaf77c0a38cca853e94
George Tuli <gtuli6960@sohamcollege.org.uk>
2014-05-08 17:35:05 +0100

    Improve greenBottles
    Add an input variable to ask how many bottles to have hanging on the wall to begin with.  Also improve so the correct language is used when referring to '1 green bottle'.

--- greenBottles.py
+++ greenBottles.py
@@ -1,7 +1,17 @@
 import time
-for i in range(10,0,-1):
-    for j in range(2):
-        print("{0} green bottles hangning on the wall,".format(i))
-    print("And if 1 green bottle should accidently fall,")
-    print("There'll be {0} green bottles hanging on the wall.\n".format(i-1))
+bottles = int(input("How many green bottles are hanging on the wall?: "))
+for i in range(bottles,0,-1):
+    if i == 1:
+        for j in range(2):
+            print("1 green bottle hanging on the wall,")
+        print("And if 1 green bottle should accidently fall,")
+        print("There'll be 0 green bottles hanging on the wall.\n")
+    else:
+        for j in range(2):
+            print("{0} green bottles hangning on the wall,".format(i))
+        print("And if 1 green bottle should accidently fall,")
+        if i == 2:
+            print("There'll be 1 green bottle hanging on the wall.\n".format(i-1))
+        else:
+            print("There'll be {0} green bottles hanging on the wall.\n".format(i-1))
     time.sleep(1)
